5d2c06d94d157bc1836028f54050be79364f8810
“elinamat” <elinamathe@gmail.com>
2022-06-01 13:36:57 +0200

    033 bike shop v2 - circular import error -  still needs a lot of work!!

--- rent.py
+++ rent.py
@@ -1,3 +1,5 @@
+from db import bikes
+
 class Person:
     def __init__(self, name: str):
         self.name = name
@@ -8,41 +10,38 @@ class Partner(Person):
         """
         Here I instantiate 'Partner' Objects!
         """
-        # super().__init__()
+        super().__init__(name)
         self.name = name
         self.bike = bike
 
+""" THE USER INPUT AS ARGUMENT IN __INIT__ DOESN'T WORK 
+    @classmethod
+    def get_user_input(self):
+        while 1:
+            try:
+                name = input("Give your name")
+                return self(name)
+            except:
+                print('Invalid input!')
+                continue
+
+    def show_full_name(self):
+        return self.name
+"""
 
 class Client(Person):
     def __init__(self, name: str):
         """
         Here I instantiate 'Client' Objects!
         """
-        # super().__init__()
-        self.name = input("Give your name")
+        super().__init__(name)
         print(f"Welcome {self.name}! How would you like to search for a bike?!")
-        print("1. By age")
-        print("2. By manufacturer")
-        print("3. List Type")
-        print("0. Exit")
-        self.bikeselection = input("Type an option: ")
-        choose_bike(self.bikeselection)
-
-    def choose_bike(self, bikeoption):
-        self.bikeoption = bikeoption
-        while option != 0:
-            if option == 1:
-                usr_desired_age = input("Enter the desired bike age")
-                bike for bike in bikes:
-                    if usr_desired_age == bike.age
-                        print(bikes[bike])
-
-            elif option == 2:
-                pass
-            elif option == 3:
-                pass
-
-
+        # print("1. By age")
+        # print("2. By manufacturer")
+        # print("3. List Type")
+        # print("0. Exit")
+        # self.bikeselection = input("Type an option: ")
+        # choose_bike(self.bikeselection)
 
 """
         for i in
@@ -51,7 +50,7 @@ class Client(Person):
         self.bike
 """
 
-class Bike():
+class Bike:
     def __init__(self, number, modelname, owner, age, manufacturer, type_):
         self.modelname = modelname
         self.owner = owner
@@ -73,8 +72,19 @@ class RentManagement:
 
         pass
 
-    def rent(self):
-        pass
+    def rent_bike(self, bikeoption):
+        self.bikeoption = bikeoption
+        while bikeoption != 0:
+            if bikeoption == 1:
+                usr_desired_age = int(input("Enter the desired bike age: "))
+                print(f"Available bikes based on your critiria: ")
+                for bike in bikes:
+                    if usr_desired_age == bike.age:
+                        print(bikes[bike])
+            elif option == 2:
+                pass
+            elif option == 3:
+                pass
 
     def return_bycicle(self):
         # mark the bicycle  as AVAILABLE
